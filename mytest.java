e4b891be6cda9d9183b97416aa8d39a0b6f98756
zyx <2106418996@qq.com>
2020-08-18 18:23:52 +0800

    制造冲突2

--- mytest.java
+++ mytest.java
@@ -2,5 +2,6 @@ public void Hello() {
 	public static void main(String[] args) {
 		System.out.println("Hello World);
 		System.out.println("Hello World1111);
+		System.out.println("Hello World5555);
 	}
 }
